674b01899342827dbb40a89173c4d8d528a747f7
101506002 <amy830221@gmail.com>
2018-06-24 15:26:28 +0900

    Added utils.js and removed functions folder. renamed:    src/js/functions/debounce.js -> src/js/utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-function debounce(func, wait, immediate) {
+export function debounce(func, wait, immediate) {
     var timeout;
     return function() {
     var context = this, args = arguments;
@@ -8,6 +8,4 @@ function debounce(func, wait, immediate) {
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
       };
-    };
-
-module.exports = debounce;
+};
